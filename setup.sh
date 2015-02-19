8475b509d3614be602530b855ef5001d0e5da5ba
hachy <1613863+hachy@users.noreply.github.com>
2015-02-19 14:00:48 +0900

    add .pryrc

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
-#!/bin/zsh
+#!/bin/bash
 
-DOT_FILES=( .gitconfig .gitignore .gitignore_global .gvimrc .vimrc .vim .zshrc .gemrc .tmux.conf )
+DOT_FILES=( .gitconfig .gitignore .gitignore_global .gvimrc .vimrc .vim .zshrc .gemrc .tmux.conf .pryrc )
 
-white="\033[0m"
+reset="\033[0m"
 magenta="\033[1;35m"
 cyan="\033[1;36m"
 
 for file in ${DOT_FILES[@]}
 do
   if [ -L $HOME/$file ]; then
-    echo "既に${magenta} $file ${white} のシンボリックリンクが存在するょ"
+    echo -e "既に${magenta} $file ${reset} のシンボリックリンクが存在するょ"
   else
     ln -s $HOME/dotfiles/$file $HOME/$file
-    echo "${cyan} $file ${white} のシンボリックリンクをはったょ"
+    echo -e "${cyan} $file ${reset} のシンボリックリンクをはったょ"
   fi
 done
