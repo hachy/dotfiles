265b18e0ca554724561ac5b0d6d042ee8dbd6299
hachy <1613863+hachy@users.noreply.github.com>
2021-04-17 23:30:40 +0900

    add alacritty settings
    and fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .gitconfig .gitignore .gitignore_global .zshrc .zshrc.local .gemrc .tmux.conf .pryrc )
+DOT_FILES=( .gitconfig .gitignore .gitignore_global .zshrc .gemrc .tmux.conf .pryrc )
 
 reset="\033[0m"
 magenta="\033[1;35m"
@@ -9,16 +9,23 @@ cyan="\033[1;36m"
 for file in ${DOT_FILES[@]}
 do
   if [ -L $HOME/$file ]; then
-    echo -e "既に${magenta} $file ${reset} のシンボリックリンクが存在するょ"
+    echo -e "${magenta}$file${reset} already exists."
   else
     ln -s $HOME/dotfiles/$file $HOME/$file
-    echo -e "${cyan} $file ${reset} のシンボリックリンクをはったょ"
+    echo -e "Create symlink for ${cyan}$file${reset}"
   fi
 done
 
-if [ -L ~/.config/nvim ]; then
-  echo -e "既に${magenta} .config/nvim ${reset} のシンボリックリンクが存在するょ"
+if [ -L $HOME/.config/nvim ]; then
+  echo -e "${magenta}.config/nvim${reset} already exists."
 else
-  ln -s ~/dotfiles/.vim ~/.config/nvim
-  echo -e "${cyan} .config/nvim ${reset} のシンボリックリンクをはったょ"
+  ln -s $PWD/.vim $HOME/.config/nvim
+  echo -e "Create symlink for ${cyan}.config/nvim${reset}"
+fi
+
+if [ -L $HOME/.config/alacritty ]; then
+  echo -e "${magenta}.config/alacritty${reset} already exists."
+else
+  ln -s $PWD/config/alacritty $HOME/.config/alacritty
+  echo -e "Create symlink for ${cyan}.config/alacritty${reset}"
 fi
