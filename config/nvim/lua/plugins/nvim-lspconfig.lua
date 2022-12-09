1e993308c425395699a24bac8d70e72add7e4556
hachy <1613863+hachy@users.noreply.github.com>
2022-12-09 22:08:03 +0900

    show diagnostics in hover instead of message area

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -17,6 +17,21 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
   vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
   vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+
+  vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        border = "rounded",
+        source = "always",
+        prefix = " ",
+        scope = "cursor",
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end,
+  })
 end
 
 local servers = { "solargraph", "pylsp", "gdscript", "tsserver" }
@@ -71,34 +86,3 @@ vim.diagnostic.config {
   update_in_insert = false,
   severity_sort = false,
 }
-
-local hl_tbl = {
-  [vim.diagnostic.severity.ERROR] = "DiagnosticError",
-  [vim.diagnostic.severity.WARN] = "DiagnosticWarn",
-  [vim.diagnostic.severity.HINT] = "DiagnosticHint",
-  [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
-}
-
-function PrintDiagnostics(opts, bufnr, line_nr, client_id)
-  bufnr = bufnr or 0
-  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-  opts = opts or { ["lnum"] = line_nr }
-
-  local line_diagnostics = vim.diagnostic.get(bufnr, opts)
-  if vim.tbl_isempty(line_diagnostics) then
-    return
-  end
-
-  local diagnostic_message = ""
-  local hl = "Normal"
-  for i, diagnostic in ipairs(line_diagnostics) do
-    diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
-    if i ~= #line_diagnostics then
-      diagnostic_message = diagnostic_message .. "\n"
-    end
-    hl = hl_tbl[diagnostic.severity]
-  end
-  vim.api.nvim_echo({ { diagnostic_message, hl } }, false, {})
-end
-
-vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
