1ba6f2f54150c79b124578b5bb7066005d4d1219
hachy <1613863+hachy@users.noreply.github.com>
2023-04-10 09:46:44 +0900

    fix: no need to set group inside ftdetect dir ?

--- config/nvim/ftdetect/json.lua
+++ config/nvim/ftdetect/json.lua
@@ -1,6 +1,4 @@
-local id = vim.api.nvim_create_augroup("MyGroup", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = { ".stylelintrc" },
   command = "setfiletype json",
-  group = id,
 })
