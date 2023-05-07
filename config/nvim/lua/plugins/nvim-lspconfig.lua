63523ed27a89f9c95a58f9691efee24e70464d4f
hachy <1613863+hachy@users.noreply.github.com>
2023-05-07 10:04:25 +0900

    fix: lsp diagnostics icon

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -91,7 +91,7 @@ for _, lsp in ipairs(servers_2) do
   }
 end
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
