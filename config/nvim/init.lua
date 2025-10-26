128efe8cb920e77b67de5e81e6f627707a0086f0
hachy <1613863+hachy@users.noreply.github.com>
2025-10-26 13:02:03 +0900

    fix: LuaSnip version

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -108,6 +108,7 @@ local lazy_plugins = {
 
   {
     "L3MON4D3/LuaSnip",
+    version = "v2.*",
     build = "make install_jsregexp",
     event = "VeryLazy",
     dependencies = { "rafamadriz/friendly-snippets" },
